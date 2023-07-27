0874bf51b62c6c22a6c9f53ee7edee95d778349f
Danielle <rufus6410@gmail.com>
2023-07-27 02:22:13 -0500

    Add confirmation box when takeoff button is clicked

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init () {
+    const takeOff = document.getElementById("takeoff");
+    const statusPrompt = document.getElementById("flightStatus");
+
+    takeOff.addEventListener("click", function() {
+        let confirmation = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+        if (confirmation) {
+            statusPrompt.innerHTML = "Shuttle in flight.";
+        }
+    });
+}
+
 window.addEventListener("load", init);
